c1008079d1b50fe964fe77217438dca29064bdce
Vanessa Denny <vanessaidenny@hotmail.com>
2020-10-30 14:03:23 -0300

    refactor: Adding feature and removing repeated lines

--- BowlingGameScore.Tests/BowlingGameScore_CalculateScore.cs
+++ BowlingGameScore.Tests/BowlingGameScore_CalculateScore.cs
@@ -6,73 +6,69 @@ namespace BowlingGameScore.Tests
 {
     public class BowlingGameScore_CalculateScore
     {
+        BowlingGame game = new BowlingGame();
+        List<int[]> scoreBoard = new List<int[]>();
+
         [Fact]
         public void CalculateScore_Input0_Return0()
         {
-            BowlingGame game = new BowlingGame();
-            List<int[]> scoreBoard = new List<int[]>();
-            while(scoreBoard.Count<9)
-            {
-                scoreBoard.Add(new int[] {0, 0});              
-            }
-            scoreBoard.Add(new int[] {0, 0, 0});
+            addListRound(0, 0, 9);
+            addLastRoundWithExtra(0, 0, 0);
             Assert.Equal(0, game.CalculateScore(scoreBoard));
         }
 
         [Fact]
         public void CalculateScore_Input1_Return1()
         {
-            BowlingGame game = new BowlingGame();
-            List<int[]> scoreBoard = new List<int[]>();
-            while(scoreBoard.Count<9)
-            {
-                scoreBoard.Add(new int[] {1, 1});              
-            }
-            scoreBoard.Add(new int[] {1, 1, 0});
+            addListRound(1, 1, 9);
+            addLastRoundWithExtra(1, 1, 0);
             Assert.Equal(20, game.CalculateScore(scoreBoard));
         }
 
         [Fact]
         public void CalculateScore_Input4P_6_Return19()
         {
-            BowlingGame game = new BowlingGame();
-            List<int[]> scoreBoard = new List<int[]>();
-            scoreBoard.Add(new int[] {4, 6});
-            scoreBoard.Add(new int[] {3, 0});
-            while(scoreBoard.Count<9)
-            {
-                scoreBoard.Add(new int[] {0, 0});              
-            }
-            scoreBoard.Add(new int[] {0, 0, 0});
+            addRound(4, 6);
+            addRound(3, 0);
+            addListRound(0, 0, 9);
+            addLastRoundWithExtra(0, 0, 0);
             Assert.Equal(19, game.CalculateScore(scoreBoard));
         }
 
         [Fact]
         public void CalculateScore_Input10_4_6_Return31()
         {
-            BowlingGame game = new BowlingGame();
-            List<int[]> scoreBoard = new List<int[]>();
-            scoreBoard.Add(new int[] {10, 0});
-            scoreBoard.Add(new int[] {3, 4});
-            while(scoreBoard.Count<9)
-            {
-                scoreBoard.Add(new int[] {0, 0});              
-            }
-            scoreBoard.Add(new int[] {0, 0, 0});
+            addRound(10, 0);
+            addRound(3, 4);
+            addListRound(0, 0, 9);
+            addLastRoundWithExtra(0, 0, 0);
             Assert.Equal(31, game.CalculateScore(scoreBoard));
         }
 
         [Fact]
         public void CalculateScore_Input12Strike_Return300()
          {
-            BowlingGame game = new BowlingGame();
-            List<int[]> scoreBoard = new List<int[]>();
-            while(scoreBoard.Count<9)
-            {
-                scoreBoard.Add(new int[] {10, 0});              
-            }
-            scoreBoard.Add(new int[] {10, 10, 10});
+            addListRound(10, 0, 9);
+            addLastRoundWithExtra(10, 10, 10);
             Assert.Equal(300, game.CalculateScore(scoreBoard));
+        }
+        
+        private void addRound(int firstThrow, int secondThrow)
+        {
+            scoreBoard.Add(new int[] {firstThrow, secondThrow});
+        }
+
+        private void addListRound(int firstThrow, int secondThrow, int stopAtRound)
+        {
+            while(scoreBoard.Count<stopAtRound)
+            {
+                addRound(firstThrow,secondThrow);             
+            }
+        }
+
+        private void addLastRoundWithExtra(int firstThrow, int secondThrow, int thirdThrow)
+        {
+            scoreBoard.Add(new int[] {firstThrow, secondThrow, thirdThrow});
         }
     }
 }
