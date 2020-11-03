392179b4a0b3c1a2ac017b897f4de9d8dc47739f
Vanessa Denny <vanessaidenny@hotmail.com>
2020-11-03 13:27:19 -0300

    test: All ones game - score twenty expected

--- BowlingGameScore.Tests/BowlingGameScore_CalculateScore.cs
+++ BowlingGameScore.Tests/BowlingGameScore_CalculateScore.cs
@@ -9,13 +9,19 @@ namespace BowlingGameScore.Tests
         BowlingGame game = new BowlingGame();
 
         [Fact]
-        public void CalculateScore_GutterGameScoreZero()
+        public void CalculateScore_GutterGame_ScoreZero()
         {
             addListRound(0, 20);
             Assert.Equal(0, game.CalculateScore());
         }
 
-
+        [Fact]
+        public void CalculateScore_AllOnesGame_ScoreTwenty()
+        {
+            addListRound(1, 20);
+            Assert.Equal(20, game.CalculateScore());
+        }
+        
         private void addListRound(int repeatThrow, int pins)
         {
             for (int i = 0; i < repeatThrow; i++)
