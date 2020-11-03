47359d93d504f26e130495c0faf7867e3ff39ad7
Vanessa Denny <vanessaidenny@hotmail.com>
2020-11-03 13:56:09 -0300

    feat: Spare score

--- BowlingGameScore/BowlingGame.cs
+++ BowlingGameScore/BowlingGame.cs
@@ -20,8 +20,14 @@ namespace BowlingGameScore
             int i = 0;
             for (int round = 0; round < Config.MAXROUND; round++)
             {
-                score += rolls[i] + rolls[i+1];
-                i += 2;
+                if (rolls[i] + rolls[i+1] == 10) //spare
+                {
+                    score += 10 + rolls[i+2];
+                    i += 2;
+                } else {
+                    score += rolls[i] + rolls[i+1];
+                    i += 2;
+                }
             }
             return score;
         }
