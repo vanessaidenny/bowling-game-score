327882b9e8b12505a510937ceda2e93123f00603
Vanessa Denny <vanessaidenny@hotmail.com>
2020-11-03 13:26:48 -0300

    feat: All ones game

--- BowlingGameScore/BowlingGame.cs
+++ BowlingGameScore/BowlingGame.cs
@@ -5,15 +5,17 @@ using System.Linq;
 namespace BowlingGameScore
 {
     public class BowlingGame
-    {        
+    {
+        private int score = 0;
+        
         public void ThrowBall(int pins)
         {
-            
+            this.score += pins;
         }
 
         public int CalculateScore()
         {
-            return 0;
+            return score;
         }
     }
 }
